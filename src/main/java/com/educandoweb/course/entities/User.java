78f21189ce8ab3eb149f366f72b19fae3de6fcb4
Leandro Gustavo Neis <lgn200@gmail.com>
2021-07-14 00:25:15 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,22 @@
+package com.educandoweb.course.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+}
+
