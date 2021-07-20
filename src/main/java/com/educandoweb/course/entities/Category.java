76585c71516748d9d439d92b7dbf0a4a2fde91bb
Leandro Gustavo Neis <lgn200@gmail.com>
2021-07-20 14:45:08 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -18,6 +21,9 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -42,6 +48,10 @@ public class Category implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
@@ -73,8 +83,4 @@ public class Category implements Serializable {
 	public String toString() {
 		return "Category [id=" + id + ", nome=" + nome + "]";
 	}
-	
-	
-	
-
 }
