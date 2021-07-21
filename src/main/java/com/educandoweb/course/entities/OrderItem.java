c073d5790816509024c29d1ee5110f9d104da81b
Leandro Gustavo Neis <lgn200@gmail.com>
2021-07-21 16:54:19 -0300

    Implement OrderItemPk - PART 1;

---
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+public class OrderItem implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+
+}
---
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,41 @@
+package com.educandoweb.course.entities.pk;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
+
+import java.util.Objects;
+
+public class OrderItemPK {
+
+    private Order order;
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK)) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return order.equals(that.order) && product.equals(that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
