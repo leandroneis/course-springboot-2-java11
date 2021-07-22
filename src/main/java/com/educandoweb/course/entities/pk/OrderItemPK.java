282385581706e07a39cc8754cb7f722478b223fe
Leandro Gustavo Neis <lgn200@gmail.com>
2021-07-22 00:41:06 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -1,13 +1,24 @@
 package com.educandoweb.course.entities.pk;
 
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.Product;
 
-import java.util.Objects;
+@Embeddable
+public class OrderItemPK implements Serializable{
 
-public class OrderItemPK {
+	private static final long serialVersionUID = 1L;
 
+	@ManyToOne
+	@JoinColumn(name = "order_id")
     private Order order;
+	@ManyToOne
+	@JoinColumn(name = "product_id")
     private Product product;
 
     public Order getOrder() {
@@ -26,16 +37,36 @@ public class OrderItemPK {
         this.product = product;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof OrderItemPK)) return false;
-        OrderItemPK that = (OrderItemPK) o;
-        return order.equals(that.order) && product.equals(that.product);
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((order == null) ? 0 : order.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(order, product);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		if (order == null) {
+			if (other.order != null)
+				return false;
+		} else if (!order.equals(other.order))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
+	}
+
+    
 }
