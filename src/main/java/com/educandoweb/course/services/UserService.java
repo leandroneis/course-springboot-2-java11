12cf4e6f46a37b8f23759f4bbc2263c8fea628a8
Leandro Gustavo Neis <lgn200@gmail.com>
2021-07-22 17:27:47 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -22,5 +22,9 @@ public class UserService {
 	public User findById(Long id) {
 		return repository.findById(id).get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 
 }
