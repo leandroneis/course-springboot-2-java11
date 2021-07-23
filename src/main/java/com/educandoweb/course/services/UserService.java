b32ad0065124938d2fbec9ea5f44385d1b9f31d0
Leandro Gustavo Neis <lgn200@gmail.com>
2021-07-23 17:58:40 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 		repository.deleteById(id);
 	}
 
+	public User update(Long Id, User obj) {
+		User entity = repository.getById(Id);
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
 }
